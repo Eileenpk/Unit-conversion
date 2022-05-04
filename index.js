b859069be7b2a2d2e25ad595211af4eb672543c4
Eileenpk <eileenrkerekes@gmail.com>
2022-05-04 11:01:53 -0400

    Add files via upload
     I updated the code to be able to take an input and do the conversions for the number being put in. I rewrote all the code from scratch without looking at the original version. It was interesting to see the difference in the code.

--- index.js
+++ index.js
@@ -1,27 +1,58 @@
-let unit = 20
-let meterToFoot = unit * 3.280
-let mtfr = meterToFoot.toFixed(3)
-let footToMeter = unit / 3.280
-let ftmr = footToMeter.toFixed(3)
-let litersToGallons = unit * 0.264
-let ltgr = litersToGallons.toFixed(3)
-let gallonsToLiters = unit / 0.264
-let gtlr = gallonsToLiters.toFixed(3)
-let kilogramToPounds = unit * 2.204
-let ktpr = kilogramToPounds.toFixed(3)
-let poundsToKilograms = unit / 2.204
-let ptkr = poundsToKilograms.toFixed(3)
-let lengthEl = document.getElementById('length-el');
-let volumeEl = document.getElementById('volume-el');
-let massEl = document.getElementById('mass-el');
-let input = document.getElementById('input'); 
+let input = document.getElementById('input')
+let inputArr = []
+const meterToFeet = document.getElementById("meter-to-feet")
+const literToGallons = document.getElementById("liters-to-gallons")
+const kiloToPounds = document.getElementById("kilo-to-pounds")
+const convertBtn = document.getElementById('convert-btn') 
 
+convertBtn.addEventListener('click', function(){
+    inputArr.pop()
+    inputArr.push(input.value)
+    metersToFeet()
+    feetToMeters()
+    litersToGallons ()
+    gallonsToLiters()
+    kilosToPounds()
+    poundsToKilos()
+    render(meterToFeet, 'meters', 'feet', feetOutPut, meterOutPut)
+    render(literToGallons, 'liters', 'gallons', gallonOutPut, literOutPut )
+    render(kiloToPounds, 'kilos', 'pounds', poundOutPut, kiloOutPut)
+})
 
-function convert(){
-    lengthEl.textContent = unit + ' Meters = ' + mtfr + ' feet | ' + unit + ' Feet  = ' + ftmr + ' Meters'
-
-    volumeEl.textContent = unit + ' Liters = ' + ltgr + ' Gallons | ' + unit + ' Gallons  = ' + gtlr+ ' Liters'
-
-    massEl.textContent = unit + ' Kilos = ' + ktpr + ' Pounds | ' + unit + ' Pounds  = ' + ptkr + ' Kilos'
-    console.log(convert)
+// render input to output el
+function render(container,unit1, unit2, outPut1, outPut2) {
+    container.textContent = `${inputArr} ${unit1} = ${outPut1} ${unit2} | ${inputArr} ${unit2} = ${outPut2} ${unit1}`
 }
+
+// // calc meters to feet
+function metersToFeet() {
+   fop = input.value * 3.281 
+  feetOutPut = fop.toFixed(3)
+}
+//calc feet to meters
+function feetToMeters() {
+    mop = input.value / 3.281
+    meterOutPut = mop.toFixed(3)
+}
+// calc liters to gallons
+function litersToGallons () {
+   gop = input.value / 3.785
+    gallonOutPut = gop.toFixed(3)
+}
+// calc gallons to liters
+function gallonsToLiters() {
+   lop = input.value * 3.785
+   literOutPut = lop.toFixed(3)
+}
+// calc kilos to pounds
+function kilosToPounds() {
+   pop = input.value * 2.205
+   poundOutPut = pop.toFixed(3)
+}
+// calc pounds to kilos
+function poundsToKilos() {
+   kop = input.value / 2.205
+   kiloOutPut = kop.toFixed(3)
+}
+
+
